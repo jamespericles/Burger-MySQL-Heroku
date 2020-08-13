04c6b62b12638d05130dfceb96592aacb48f2162
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-13 18:04:23 -0400

    began working, finsihed writing and exporting connection

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+const handlebars = require("express-handlebars");
+
+app.set("view engine", "handlebars");
+
+app.engine(
+  "handlebars",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("index", { layout: "index" });
+});
