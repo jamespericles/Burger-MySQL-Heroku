fa9a1d9bb11f4e9cc0f3bd17b5f9ac55ca058ebb
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-15 19:08:31 -0400

    reformatted schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS chfgklez6l8fh36t;
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE chfgklez6l8fh36t;
+CREATE DATABASE burgers_db;
 
-Use chfgklez6l8fh36t;
+USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(50),
     devoured BOOLEAN,
     PRIMARY KEY(id)
-)
+);
