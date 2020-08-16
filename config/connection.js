03e8345910164b194c5bf5fd2c80f71689c2f81e
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-08-15 20:54:45 -0400

    reformatted the connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   console.log("no jaws");
   connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: 3300,
+    port: 3000,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
